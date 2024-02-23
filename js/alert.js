4f404ca684ad35e6e073ec72974254b31ccd982a
“rubng87” <gallegoruben_srye@hotmail.com>
2024-02-23 12:28:37 +0100

    23.02

--- js/alert.js
+++ js/alert.js
@@ -2,7 +2,7 @@ let num1 = parseInt(prompt("Indica el primer número"))
 let num2 = parseInt(prompt("Indica el segundo número"))
 console.log(typeof(num1))
 let suma = num1 + num2
-let aviso = "La suma es" + suma
+let aviso = "La suma es " + suma
 
 alert(aviso)
 
