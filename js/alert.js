590205b226a1ec11f37c633029b1358c6c0d4721
“rubng87” <gallegoruben_srye@hotmail.com>
2024-02-23 13:42:04 +0100

    23.02

--- js/alert.js
+++ js/alert.js
@@ -1,11 +1,22 @@
-let num1 = parseInt(prompt("Indica el primer número"))
-let num2 = parseInt(prompt("Indica el segundo número"))
+let nombre = prompt("¿Cúal es tu nombre?")
+alert("Bienvenido " + nombre)
+let num1 = parseInt(prompt("Vamos a sumar, dime el primer número"))
+let num2 = parseInt(prompt("I ahora el segundo número y te lo sumaré sea el que sea :)"))
 console.log(typeof(num1))
 let suma = num1 + num2
-let aviso = "La suma es " + suma
+// let aviso = "La suma es " + suma + nombre
 
-alert(aviso)
+// alert(aviso)
 
-console.log("La suma es", suma)
+// console.log("La suma es", suma, nombre)
 
-alert(`La suma es ${num1 + num2}`)
+alert(`La suma es ${num1 + num2}` + " " + nombre)
+// confirm("¿Está lloviendo?")
+
+
+var resultado = window.confirm('¿Está lloviendo?');
+if (resultado === true) {
+  window.alert("Pues coje el paraguas, no te mojes " + nombre);
+} else {
+  window.alert("pues no cojas el paraguas " + nombre);
+}
