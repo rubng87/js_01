f8d02fb15b863ebc63fd6e78c7b1022771466fd1
“rubng87” <gallegoruben_srye@hotmail.com>
2024-02-23 12:11:47 +0100

    23.02

---
+++ js/alert.js
@@ -0,0 +1,11 @@
+let num1 = parseInt(prompt("Indica el primer número"))
+let num2 = parseInt(prompt("Indica el segundo número"))
+console.log(typeof(num1))
+let suma = num1 + num2
+let aviso = "La suma es" + suma
+
+alert(aviso)
+
+console.log("La suma es", suma)
+
+alert(`La suma es ${num1 + num2}`)
---
+++ js/script.js
@@ -0,0 +1,41 @@
+/*Este es mi primer programa en Javascript
+        A ver que tal va */
+        // Este es un comentario de una linea
+        let primeraVariable = "Buenos días" //Es también es un comentario
+        var segundaVariable = "Son las 9"
+        terceraVariable = "Adiós" //No lo hagas
+        const PI = 3.1416
+
+        let HOLA = "Hola"
+        let hola = "Hola"
+        let hOLa = "Hola"
+
+        segundaVariable = 'Son las "9:30"'
+        primeraVariable = "Buenas tardes"
+        // PI = 18
+
+        primeraVariable  = "25"
+
+        let palabra = "H" + "O" + "L" + "A" // HOLA
+        let suma = 10 + 20 // 30
+        let suma2 = "10" + "20" // 1020
+
+        let verdadero = true
+        let falso = false
+
+        
+        console.log(typeof(palabra))
+
+
+        let segunda_variable = 1
+        let nombre = "L'avinguda" + " Rubén"
+       
+        // alert(primeraVariable)
+        console.log(segundaVariable);
+
+
+        // nombre = prompt("¿Cúal es tu nombre?")
+        // alert("Saludos, " + nombre)
+
+        let llueve = confirm("¿Está lloviendo ahora?")
+        alert(llueve)
